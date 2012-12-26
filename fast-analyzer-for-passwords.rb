3f4ffce40e582da14ae7485201cf2548cd2951e7
Felipe Molina <felmoltor@gmail.com>
2012-12-26 12:44:46 +0100

    Añadido el grafico de top passwords (todavia no correcto)

--- fast-analyzer-for-passwords.rb
+++ fast-analyzer-for-passwords.rb
@@ -29,6 +29,8 @@ else
     gruffinstalled = false
 end
 
+$executablepath = File.expand_path File.dirname(__FILE__)
+
 # TODO: Añadir modulo para guardar estadísticas en un sqlite (debe preguntar pais o lenguaje del dump, perfil del dump [clasificación de los usuarios, clasificación del dump; tienda on line, web privada, acceso privado de admins, etc...])
 # TODO: Añadir modulo para contar las repeticiones de letras y números
 # TODO: Añadir modulo para contar las password que son fechas con diferentes formatos
@@ -36,11 +38,12 @@ end
 # TODO: Añadir modulo para contar las contraseñas que contienen un listado de ciudades de un fichero
 # TODO: Añadir modulo para contar las contraseñas que contienen el nombre de usuario
 # TODO: Añadir opcion vervose para mostrar porcentaje de progreso con nivel 1 y password que analiza con nivel 2
+# TODO: Añadir modulo para dar una puntuación de 1 a 10 la "salud" de la base de datos de contraseñas
 
 #====================
 
 def showBanner
-  bannerf = File.open("banner.txt","r")
+  bannerf = File.open("#{$executablepath}/banner.txt","r")
   while (line = bannerf.gets)
     puts line
   end  
@@ -49,7 +52,7 @@ end
 #====================
 
 def parseOptions
-    opts = {:pwdfile => nil, :ntop => 10, :charstats => true, :passwdstats => true, :regexp => '^.*(passwd|pwd|password).*$',:pwdlenstats => true,:verbose =>0}
+    opts = {:pwdfile => nil, :ntop => 10, :charstats => true, :passwdstats => true, :regexp => '^.*(passwd|pwd|password).*$',:pwdlenstats => true}
     parser = OptionParser.new do |opt|
         opt.banner = "Usage: #{__FILE__} [options] -f <password-file>"
         opt.separator ""
@@ -73,9 +76,6 @@ def parseOptions
         opt.on('-E [REGEXP]','--regexp [REGEXP]','Search the specified regular expression within the password file (default is "^.*(passwd|pwd|password).*$")') do |regexp|
             opts[:regexp] = regexp
         end
-   #     opt.on('-v (0,1,2,3)','--verbose (0,1,2,3)',:w,'Level of verbosity (1: Display percentage, 2: Display 1 of every 10 analyzed passwords, 3: Display every password analyzed') do |level|
-     #       opts[:verbose] = level
-    #    end
         opt.on("-h","--help", "Print help and usage information") do
             puts parser
             exit
@@ -148,7 +148,12 @@ end
 def isOnlyNumbers(pwd)
   !(pwd.index(/[0-9]+/).nil?) and (pwd.index(/^a-zA-Z0-9/).nil? and pwd.index(/[A-Z]+/).nil? and pwd.index(/[a-z]+/).nil?)
 end
+
 #============
+
+def getCharRepetitions(pwd)
+  # Every char
+end
 
 #====================================
 #====== MAIN ========================
@@ -175,6 +180,7 @@ ntop = 10
 len_histogram = nil
 strength_histogram = nil
 pwd_counter = {}
+pwd_hist_ntop = {}
 contains_regexp_pwd = []
 i = 1
 
@@ -191,7 +197,7 @@ f = File.open(options[:pwdfile],"r")
 f.each_line{|pwd|
   pwd.strip!
   npwd += 1
-
+  
   if (isComplex(pwd))
     complex +=1
   elsif (isUpperLowerNums(pwd))
@@ -211,20 +217,20 @@ f.each_line{|pwd|
   else
     other += 1
   end
-
+  
   if containsRegexp(pwd,options[:regexp])
     containsregexp += 1
     contains_regexp_pwd << pwd
   end
-
+  
   lenhist[pwd.size] += 1
-
-# Save count of user passwords
-if pwd_counter.keys.include?(pwd)
-    pwd_counter[pwd] += 1
-else
-    pwd_counter[pwd] = 1
-end
+  
+  # Save count of user passwords
+  if pwd_counter.keys.include?(pwd)
+      pwd_counter[pwd] += 1
+  else
+      pwd_counter[pwd] = 1
+  end
 }
 
 puts "= RESULTS ="
@@ -264,7 +270,7 @@ if gruffinstalled
     strength_histogram.data("Numbers",(onlynumber.to_f*100/npwd).round(2))
     strength_histogram.data("Others",(other.to_f*100/npwd).round(2))
 
-    strength_histogram.write("password-strength-histogram.png")
+    strength_histogram.write("#{options[:pwdfile]}-password-strength.png")
 end
 
 puts "============"
@@ -296,7 +302,7 @@ if gruffinstalled
       labelindex += 1 
     } 
     len_histogram.labels = x_labels
-    len_histogram.write('password-length-histogram.png')
+    len_histogram.write("#{options[:pwdfile]}-password-length.png")
 end
 puts "============"
 
@@ -307,10 +313,32 @@ puts "= Top #{options[:ntop]} passwords ="
 pwd_hist = pwd_counter.sort_by{|k,v| v}.reverse
 pwd_hist.each {|pass,nrepetitions|
   puts "#{i} - #{pass}: #{nrepetitions}"
+  pwd_hist_ntop[pass] = nrepetitions
   if i >= options[:ntop]
     break
   end
   i += 1
 }
+
+if gruffinstalled
+    y_labels = {}
+    labelindex = 0
+  
+    top_histogram = Gruff::SideBar.new
+    top_histogram.title = "Top #{options[:ntop]} Repeated Passwords"
+    top_histogram.y_axis_label = "Password"
+    top_histogram.x_axis_label = "Number of repetitions (of #{npwd})"
+    top_histogram.sort = false
+    top_histogram.hide_legend = true
+    # Data
+    top_histogram.data("Repeated Passwords", pwd_hist_ntop.values,"#FE5010")
+    pwd_hist_ntop.each {|pwd,nrep|
+      y_labels[labelindex] = pwd.to_s
+      labelindex += 1 
+    } 
+    top_histogram.labels = y_labels
+    top_histogram.y_axis_increment = 1
+    top_histogram.write("#{options[:pwdfile]}-password-top.png")
+end
 puts "===================="
 
