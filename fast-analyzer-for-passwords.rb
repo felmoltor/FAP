37141e113cb0619f604c53b3d18307cf4967f05f
Felipe Molina <felmoltor@gmail.com>
2014-01-31 13:01:56 +0100

    Update fast-analyzer-for-passwords.rb

--- fast-analyzer-for-passwords.rb
+++ fast-analyzer-for-passwords.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 """ 
-Copyright (C) 2012  Felipe Molina (@felmoltor, felmoltor@gmail.com)
+Copyright (C) 2012  Felipe Molina (@felmoltor)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
